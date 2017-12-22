187ff0fa14d043feaa1b0681ba955ac6229798f4
Maxime Cholon <jeuxserveurs@gmail.com>
2017-12-22 02:02:17 +0100

    V 1.7.1
    Donation Update

--- side_info/donate.py
+++ side_info/donate.py
@@ -12,10 +12,10 @@ class Donate:
     def affichage(self):
         value_test = "```css\nBitcoin : [1jc3V3T5mefuD9asa7en976NKVGssQuMq]```"
         value_ETH = "```css\n Ethereum : [abowallet.eth]```"
-        value_DOGE = "```css\nDogeCoin : [DSEaEfD4NQ68xbD92wN9UgcL66qZtwiFJB]"
-        value_BAT = "```css\nBAT : [0x670B7A9497f79Ef57BbFFFB553d979E7aD225344]"
-        value_LTC = "```css\nLitecoin : [LQS415ftVrkSjjmFUyKNVBhY1fJzFLSKaz]"
-        value_DASH = "```css\nDash : [XkbrvfjnN1geyBJVe8igNwDYVFRPNWpRuz]"
+        value_DOGE = "```css\nDogeCoin : [DSEaEfD4NQ68xbD92wN9UgcL66qZtwiFJB]```"
+        value_BAT = "```css\nBAT : [0x670B7A9497f79Ef57BbFFFB553d979E7aD225344]```"
+        value_LTC = "```css\nLitecoin : [LQS415ftVrkSjjmFUyKNVBhY1fJzFLSKaz]```"
+        value_DASH = "```css\nDash : [XkbrvfjnN1geyBJVe8igNwDYVFRPNWpRuz]```"
         embed = discord.Embed(colour=discord.Colour(self.color), url="https://discordapp.com")
         embed.set_thumbnail(url="https://files.coinmarketcap.com/static/img/coins/32x32/bitcoin.png")
         embed.add_field(name=":slot_machine: Wanna give me some money for my work ?",
